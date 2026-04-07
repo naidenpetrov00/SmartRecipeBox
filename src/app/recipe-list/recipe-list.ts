bdc64e2242de1e71720ceec182a5906c56ba8382
naidenpetrov00 <naiden.petrov.31.12.00@gmail.com>
2026-04-07 17:48:35 +0300

    Module 13

--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { MOCK_RECIPES } from '../mock-recipes';
 import { RecipeDetail } from '../recipe-detail/recipe-detail';
 import { RecipeModel } from '../models';
@@ -7,22 +8,18 @@ import { RecipeModel } from '../models';
 @Component({
   selector: 'app-recipe-list',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RecipeDetail],
+  imports: [RecipeDetail, FormsModule],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css',
 })
 export class RecipeList {
-  protected recipe = signal<RecipeModel>(MOCK_RECIPES[0]);
+  protected searchTerm = signal('');
 
-  protected nextRecipe(): void {
-    this.recipe.update((currentRecipe) => {
-      const currentIndex = MOCK_RECIPES.findIndex((recipe) => recipe.id === currentRecipe.id);
-      const nextIndex = (currentIndex + 1) % MOCK_RECIPES.length;
+  protected filteredBySearchRecipes = computed(() =>
+    MOCK_RECIPES.filter((r) => r.name.toLowerCase().includes(this.searchTerm().toLowerCase())),
+  );
 
-      return MOCK_RECIPES[nextIndex];
-    });
-  }
-  protected toogleFavouriteStatus(): void {
-    this.recipe.update((recipe) => ({ ...recipe, isFavorite: !recipe.isFavorite }));
+  protected toogleFavouriteStatus(recipe: RecipeModel): void {
+    recipe.isFavorite = !recipe.isFavorite;
   }
 }
